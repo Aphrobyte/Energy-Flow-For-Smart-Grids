e9dcdcbc0e69078a08961962e510010d6a0cb4c8
Aphrobyte <aphrodite.ioannidou@outlook.com>
2024-08-17 19:44:14 +0200

    Variable declaration complete; ready to validate

--- ServoPhotoresistorTest/ServoPhotoresistorTest.ino
+++ ServoPhotoresistorTest/ServoPhotoresistorTest.ino
@@ -20,11 +20,12 @@ bool in_b_Goto_Optimal_Servo_Pos;   //When button 4 is pressed the Photovoltaic 
 //Internal Variables
 //==========================================================//
 
-int servo_pos = 0;           
+
+int int_optimal_Servo_pos = 0;   //position of maximum (optimal) Photoresistor voltage output
 
 int int_PhotoResistor_Measured;   //Analog input of photoresistor measurements
-int max_measurement = 0; //initial voltage value
-int opt_pos = 0;         //position of maximum voltage output
+int int_Max_PhotoResistor_Measured = 0; //Maximum Voltage
+
 
 
 const int b_Scan_Servo_PhotoResistor = 3; // b_Scan_Servo_PhotoResistor is hardwared connected to digital pin 3 and is configured as digital input Read 
@@ -44,22 +45,24 @@ void setup()
   pinMode(4, INPUT); //button2 is connected to digital pin 4
 }
 void Servo_LightScan() {
-  max_measurement = 0;
-  for (servo_pos = 0; servo_pos <= 180; servo_pos += 1) //motor performs 180 degrees movement
+  int_Max_PhotoResistor_Measured = 0;
+  int int_servo_pos;
+
+  for (int_servo_pos = 0; int_servo_pos <= 180; int_servo_pos += 1) //motor performs 180 degrees movement
   {    
-    servo_PhotoResistor.write(servo_pos);
+    servo_PhotoResistor.write(int_servo_pos);
     int_PhotoResistor_Measured = analogRead(in_int_PhotoResistor_Measured);           
     //delay(15);   
 
     Serial.println("Current position: ");
-    Serial.println(servo_pos);
+    Serial.println(int_servo_pos);
 
     Serial.println("Current light: ");    
     Serial.println(int_PhotoResistor_Measured);  
   
-   if (int_PhotoResistor_Measured > max_measurement) {
-    max_measurement = int_PhotoResistor_Measured;
-    opt_pos = servo_pos; //assignment of maximum light measurement and optimal position values during servo motion
+   if (int_PhotoResistor_Measured > int_Max_PhotoResistor_Measured) {
+    int_Max_PhotoResistor_Measured = int_PhotoResistor_Measured;
+    int_optimal_Servo_pos = int_servo_pos; //assignment of maximum light measurement and optimal position values during servo motion
    }  
   
   delay(15);
@@ -67,7 +70,7 @@ void Servo_LightScan() {
 }
 
 void Go_To_Opt_Pos(){
- servo_PhotovoltaicCell.write(opt_pos); //photovoltaic cell servo moves to optimal position
+ servo_PhotovoltaicCell.write(int_optimal_Servo_pos); //photovoltaic cell servo moves to optimal position
  }
 
 
@@ -99,9 +102,9 @@ void loop() {
  if (b_Goto_Optimal_Servo_Pos_Active == true) {
     Go_To_Opt_Pos();
     Serial.println("Optimal Position: ");
-    Serial.println(opt_pos);
+    Serial.println(int_optimal_Servo_pos);
     Serial.println("Maximum light measurement: ");
-    Serial.println(max_measurement);
+    Serial.println(int_Max_PhotoResistor_Measured);
   
    b_Goto_Optimal_Servo_Pos_Active = false;
   }
